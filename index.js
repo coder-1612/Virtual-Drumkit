2370dfed99caf1224b1359bb5c1e7ee56deca3aa
Aryman Kaushik <97455739+coder-1612@users.noreply.github.com>
2024-06-27 12:21:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var buttoni = this.innerHTML;
+        play(buttoni);
+        animate(buttoni);
+});
+}
+document.addEventListener("keydown", function(event){
+   var key = event.key;
+   play(key);
+   animate(key);
+});
+function play(key){
+   switch (key) {
+      case "w":
+         var crash = new Audio("sounds/crash.mp3");
+         crash.play();
+         break;
+     
+      case "a":
+         var kick = new Audio("sounds/kick-bass.mp3");
+         kick.play();
+         break;
+      case "s":
+         var snare = new Audio("sounds/snare.mp3");
+         snare.play();
+         break;
+      case "d":
+         var tom1 = new Audio("sounds/tom-1.mp3");
+         tom1.play();
+         break;
+      case "j":
+         var tom2 = new Audio("sounds/tom-2.mp3");
+         tom2.play();
+         break;
+      case "k":
+         var tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
+         break;
+      case "l":
+         var tom4 = new Audio("sounds/tom-4.mp3");
+         tom4.play();
+         break;
+      default:
+         alert(this.innerHTML);
+         break;
+ }
+}
+function animate(key){
+   document.querySelector("." + key).classList.add("pressed");
+   setTimeout(function (){
+      document.querySelector("." + key).classList.remove("pressed");
+   }, 100);
+}
